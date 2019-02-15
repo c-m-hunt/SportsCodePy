5df8c19b4b9470827cbf07e1058976dfda9476e4
Chris Hunt <chris.hunt@hudl.com>
2019-02-15 08:15:56 +0000

    Add setup.py and restructure to subfolder

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='SportscodePy',
+    version='0.1',
+    packages=['sportscodepy',],
+    license='',
+    long_description=open('README.txt').read(),
+)
